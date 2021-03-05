239ba4b6e4a7df991320b2ec5d1fb09766f0c148
jinhaojie <jinhj4719@163.com>
2021-03-05 16:26:54 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const { addDefault } = require('@babel/helper-module-imports');
+
+module.exports = function ({ types: t }) {
+  return {
+    visitor: {
+      JSXAttribute(path, state) {
+        if (
+          t.isJSXIdentifier(path.node.name, { name: 'className' }) &&
+          t.isJSXExpressionContainer(path.node.value) &&
+          !t.isStringLiteral(path.node.value.expression) &&
+          !t.isMemberExpression(path.node.value.expression)
+        ) {
+          path.node.value = t.jsxExpressionContainer(
+            t.callExpression(
+              addDefault(path, 'classnames', { nameHint: 'classNames' }),
+              [path.node.value.expression]
+            )
+          );
+        }
+      },
+      // ASTNodeTypeHere(path, state) { }
+    }
+  };
+};
